36a3bb5adb0fe815e0289f3e5290f4475b9feae9
yu-ji <enpatsushakugan@icloud.com>
2018-05-21 17:06:15 +0900

    回転したぜ

---
+++ 4/Assets/chapter3/NewBehaviourScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    float speed = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.speed+=20;
+        }
+        
+        transform.Rotate(0, 0, this.speed);
+
+        speed *= 0.999f;
+	}
+}
